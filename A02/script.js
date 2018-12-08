c9ceed0252450d9093549743cc985774d2862c77
pluto147 <45697238+pluto147@users.noreply.github.com>
2018-12-08 11:52:05 +0400

    Update script.js

--- A02/script.js
+++ A02/script.js
@@ -1,12 +1,11 @@
 document.getElementById("highlight-product").onclick = function(){
-    var x = prompt('brendi')
-	var y = prompt('shriftis zoma')
-
+	var x = prompt('ბრენდი')
+	var y = prompt('შრიფტის ზომა')
 	var status = 0
 	for (var i = 0; i < document.getElementsByClassName('phone-list-item').length; i++) {
 		if (document.getElementsByClassName('phone-list-item')[i].getAttribute('data-brand') == x) {
 			status = 1
-            document.getElementsByClassName('phone-list-item')[i].children[0].style.fontSize = y +'px'
+            document.getElementsByClassName('phone-list-item')[i].children[0].style.fontSize = y + 'px'
 			document.getElementsByClassName('phone-list-item')[i].children[0].innerHTML = document.getElementsByClassName('phone-list-item')[i].children[0].innerHTML.bold()
 
             document.getElementsByClassName('phone-list-item')[i].children[1].style.fontSize = y + 'px'
@@ -17,6 +16,6 @@ document.getElementById("highlight-product").onclick = function(){
 
 
 	if (status == 0) {
-		alert('chanaweri ver moidzebna')
+		alert('ჩანაწერი ვერ მოიძებნა')
 	}
 }
